03f0d887741941a018319e848d0e3929864eda20
mjekrami <mjavad.ekrami@gmail.com>
2023-10-11 16:24:56 +0330

    first commit initialized micro1,2,3

---
+++ micro-1/main.py
@@ -0,0 +1,21 @@
+import os
+import uvicorn
+from fastapi import FastAPI
+
+__version__ = '0.0.1b'
+
+PORT = os.environ.get("PORT",3000)
+HOST = os.environ.get("HOST","0.0.0.0")
+
+app = FastAPI()
+
+
+@app.get("/version")
+def get_version():
+    return {"version": __version__, 
+            "name":"micro-1"}
+
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app",port=PORT, host=HOST)
